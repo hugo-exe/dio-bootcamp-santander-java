d2ddc4c5dc87f3ff830b286169ea29daea8994d7
Hugo <113729299+hugo-exe@users.noreply.github.com>
2024-05-26 17:49:56 -0300

    revisando controle de fluxo

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,7 @@
 public class ResultadoEscolar {
  public static void main(String[] args) {
   int nota = 10;
-
-  if(nota >= 7)
-  System.out.println("Aprovado");
-
-  else if (nota >= 5 && nota < 7) 
-  System.out.println("Prova de recuperação");
-  
-  else 
-  System.out.println("Reprovado");
+  String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Reprovado" : "Reprovado";
+  System.out.println(resultado);
  }
 }
