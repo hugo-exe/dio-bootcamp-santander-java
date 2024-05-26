57f7337970e8107947f19bc800f304cc045fd697
Hugo <113729299+hugo-exe@users.noreply.github.com>
2024-05-26 18:14:12 -0300

    Revisando for e array

---
+++ src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+ public static void main(String[] args) {
+  int carros;
+
+  for (carros = 1; carros <= 20; carros ++);
+   System.out.println("Contando carros " + carros);
+ }
+
+}
